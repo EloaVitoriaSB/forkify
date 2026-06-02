990d4efbbdf99f959ad0ecd68bbfcf5e6c17f024
EloaVitoriaSB <veloa0135@gmail.com>
2026-06-02 17:32:33 -0300

    ajustando a home para seguir os padroes do angular v21

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,27 +1,25 @@
-import { Component, OnDestroy, OnInit, inject, signal } from '@angular/core';
+import { Component, DestroyRef, OnDestroy, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReceitasService } from '../../core/services/receitas.service';
+import { LoadingService } from '../../core/services/loading.service';
 import { Subject, takeUntil } from 'rxjs';
 import { Receita } from '../../core/models/receita.model';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
   imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home implements OnInit, OnDestroy {
+export class Home implements OnInit{
 
+  private loadingService = inject(LoadingService);
   private receitasService = inject(ReceitasService);
-  private destroy$ = new Subject<void>()
+  destroyRef = inject(DestroyRef);
 
-  receitas= signal<Receita[]>([])
+  loading = this.loadingService.loading;
+  receitas = signal<Receita[]>([])
 
-  loading = false;
-
-  erro = '';
 
   ngOnInit(): void {
     this.buscarReceitas('pizza');
@@ -29,32 +27,20 @@ export class Home implements OnInit, OnDestroy {
 
   buscarReceitas(query: string) {
 
-    this.loading = true;
-
-    this.erro = '';
-
     this.receitasService.getTodasReceitas(query)
-      .pipe(takeUntil(this.destroy$))
       .subscribe({
         next: (res) => {
           this.receitas.set(res.data.recipes);
-
           console.log('RECEITAS', this.receitas);
 
-          this.loading = false;
+          this.destroyRef.onDestroy(() => {
+            console.log('Componente destruído, cancelando assinaturas');
+          });
         },
         error: () => {
           console.error('Erro ao carregar receitas');
-          console.log(  this.erro);
-          this.erro = 'Erro ao carregar receitas';
-          this.loading = false;
         }
       });
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
   }
 
 }
