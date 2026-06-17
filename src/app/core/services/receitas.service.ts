535e12af13c9414e5354130fe41cc04606044f21
EloaVitoriaSB <veloa0135@gmail.com>
2026-06-17 14:41:33 -0300

    get all modificado

--- src/app/core/services/receitas.service.ts
+++ src/app/core/services/receitas.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from "@angular/core";
-import { HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { environment } from "../../../environments/environment";
 import { catchError, Observable } from "rxjs";
 import { Receita } from "../models/receita.model";
@@ -14,51 +14,51 @@ import { ReceitaResponse, ReceitaDetalheResponse } from "../models/receita-respo
 
 export class ReceitasService {
 
-private http = inject(HttpClient);
+  private http = inject(HttpClient);
 
-   // pega todas as receitas
-  getTodasReceitas(query: string) : Observable<ReceitaResponse> {
-     return this.http.get<ReceitaResponse>(`${environment.apiUrl}?search=${query}`)
-     .pipe(
-      catchError((err) => {
-       console.error('Erro ao buscar receitas:', err);
-        throw err;
-      }),
-     )
+  // pega todas as receitas
+  getTodasReceitas(query: string): Observable<ReceitaResponse> {
+    return this.http.get<ReceitaResponse>(`${environment.apiUrl}?search=${query}&key=${environment.apiKey}`)
+      .pipe(
+        catchError((err) => {
+          console.error('Erro ao buscar receitas:', err);
+          throw err;
+        }),
+      )
   }
 
   // pega por ID
-  getReceitaPorId(id: string) : Observable<ReceitaDetalheResponse> {
-  return this.http.get<ReceitaDetalheResponse>(`${environment.apiUrl}/${id}`)
-  .pipe(
-    catchError((err) => {
-      console.error(`Erro ao buscar receita com ID ${id}:`, err);
-      throw err;
-    }),
-  )
+  getReceitaPorId(id: string): Observable<ReceitaDetalheResponse> {
+    return this.http.get<ReceitaDetalheResponse>(`${environment.apiUrl}/${id}`)
+      .pipe(
+        catchError((err) => {
+          console.error(`Erro ao buscar receita com ID ${id}:`, err);
+          throw err;
+        }),
+      )
   };
 
 
   //post
-  postReceita(receita: Receita) : Observable<ReceitaDetalheResponse> {
+  postReceita(receita: Receita): Observable<ReceitaDetalheResponse> {
     return this.http.post<ReceitaDetalheResponse>(`${environment.apiUrl}?key=${environment.apiKey}`, receita)
-    .pipe(
-      catchError((err) => {
-        console.error('Erro ao criar receita:', err);
-        throw err;
-      }),
-    )
+      .pipe(
+        catchError((err) => {
+          console.error('Erro ao criar receita:', err);
+          throw err;
+        }),
+      )
   };
 
 
   //delete
-  deleteReceita(id: string) : Observable<void> {
+  deleteReceita(id: string): Observable<void> {
     return this.http.delete<void>(`${environment.apiUrl}/${id}?key=${environment.apiKey}`)
-    .pipe(
-      catchError((err) => {
-        console.error(`Erro ao excluir receita com o ID ${id}:`, err);
-        throw err;
-      }),
-    )
+      .pipe(
+        catchError((err) => {
+          console.error(`Erro ao excluir receita com o ID ${id}:`, err);
+          throw err;
+        }),
+      )
   }
 }
